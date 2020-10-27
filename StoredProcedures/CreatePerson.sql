2197594a33b49f82cfbfe001ce47f8ed1853e4a6
SpectraPH <48453017+SpectraPH@users.noreply.github.com>
2020-10-27 17:21:01 +0200

    SP for getting person by email and for creating new person

---
+++ StoredProcedures/CreatePerson.sql
@@ -0,0 +1,16 @@
+CREATE OR ALTER PROCEDURE CreatePerson(
+	@role INT,
+	@Name NVARCHAR(MAX),
+	@Surname NVARCHAR(MAX),
+	@SecondName NVARCHAR(MAX),
+	@dob DATE,
+	@email NVARCHAR(MAX),
+	@password NVARCHAR(MAX)
+)
+AS
+BEGIN
+	INSERT INTO Persons
+		([Role], [Name], Surname, SecondName, DOB, Email, [Password])
+	VALUES
+		(@role, @name, @surname, @secondName, @dob, @email, @password)
+END;
