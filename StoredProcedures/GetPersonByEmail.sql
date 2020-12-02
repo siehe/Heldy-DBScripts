104cbdd6daad50f6879d7d357a513cb8851d5a3f
Пётр <petr220120017@gmail.com>
2020-12-02 21:02:28 +0200

    GetPersonsByEmail procedure updated

--- StoredProcedures/GetPersonByEmail.sql
+++ StoredProcedures/GetPersonByEmail.sql
@@ -2,7 +2,8 @@ CREATE OR ALTER PROCEDURE GetPersonByEmail
 @email NVARCHAR(MAX)
 AS
 BEGIN
-	SELECT [Id] ,[Role] ,[Name] ,[Surname] ,[SecondName] ,[DOB] ,[Email] ,[Password] 
+	SELECT [Persons].[Id] ,[Role] ,[Persons].[Name] ,[Surname] ,[SecondName] ,[DOB] ,[Email] ,[Password], [Roles].[Id] as 'RoleId'
 	FROM Persons
+	join Roles on Persons.Role = Roles.Id
 	WHERE [Email] = @email
 END;
